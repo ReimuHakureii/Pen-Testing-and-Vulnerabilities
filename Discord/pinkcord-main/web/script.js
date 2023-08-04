89933ee01d396d6fde4797bec528a194a020728c
Da-Xia Ts'ai <141364495+trifluoperazine@users.noreply.github.com>
2023-08-04 14:45:10 -0400

    Update script.js

--- Discord/pinkcord-main/web/script.js
+++ Discord/pinkcord-main/web/script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        const elements = document.querySelectorAll('[style*="display: none;"]');
-        let delay = 0;
-
-        function getRandomArbitrary(min, max) {
-            return Math.random() * (max - min) + min;
-        }
-
-        elements.forEach(element => {
-            setTimeout(() => {
-                element.style.display = "block";
-                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-            }, delay);
-            delay += getRandomArbitrary(300, 1000);
-        });
-    }, 1111);
+    showHiddenElementsWithScrollEffect();
 });
+
+function showHiddenElementsWithScrollEffect() {
+    const elements = document.querySelectorAll('[style*="display: none;"]');
+    let delay = 0;
+
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    elements.forEach(element => {
+        setTimeout(() => {
+            element.style.display = "block";
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }, delay);
+        delay += getRandomArbitrary(300, 1000);
+    });
+}
