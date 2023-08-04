32e08fcb1a1a00925ff5ca64614b13cf7f483da8
림은현 (Rim Eun-Hyeon) <63125235+ReimuHakureii@users.noreply.github.com>
2023-08-04 14:39:43 -0400

    Update bypasser.py
    encoding added

--- Discord/pinkcord-main/bypasser.py
+++ Discord/pinkcord-main/bypasser.py
@@ -1,13 +1,18 @@
 import codecs 
 import base64
 
-with open("to_bypass.txt", 'r') as f:
-    kod = f.read()
-kod = kod.encode('utf-8')
-kod = base64.b64encode(kod)
-kod = kod.decode('utf-8')
-kod = codecs.decode(kod, 'rot13')
-f.close()
-with open("to_bypass.txt", 'w') as f:
-    f.write(kod)
-print("[*] encoded")
+def encode_file(filename):
+    with open(filename, 'r') as f:
+        kod = f.read()
+
+    kod = kod.encode('utf-8')
+    kod = base64.b64encode(kod)
+    kod = kod.decode('utf-8')
+    kod = codecs.decode(kod, 'rot13')
+
+    with open(filename, 'w') as f:
+        f.write(kod)
+
+    print("[*] encoded")
+
+encode_file("to_bypass.txt")
